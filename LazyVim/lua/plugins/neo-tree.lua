0984b459d41a815d2fb4aa2310b041e9762ec1a2
TypingPenguin <Floris.kromwijk@gmail.com>
2026-06-05 23:09:10 +0200

    Some setting changes

---
+++ LazyVim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      -- Put your custom neo-tree settings here
+      filesystem = {
+        filtered_items = {
+          visible = true, -- example setting
+        },
+      },
+    },
+  },
